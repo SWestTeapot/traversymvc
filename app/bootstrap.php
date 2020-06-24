3edbae70d200bde7fb35a4acc6534f13eba42ccd
Simon W <simon@teapotcreative.co.uk>
2020-06-24 13:28:49 +0100

    4.21
    views are set
    Header, footer, css and js all being loaded by the views

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,13 +2,12 @@
 // Config
 require_once 'config/config.php';
 
-// Load Libraries 
+// Load Libraries manually 
 // require_once 'libraries/Controller.php';
 // require_once 'libraries/Core.php';
 // require_once 'libraries/Database.php';
 
 // Autoload Core Libraries
-
 spl_autoload_register(function($className){
     require_once 'libraries/' . $className . '.php';
 });
