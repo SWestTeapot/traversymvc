254148eb4fbdee66dd948ba0edcb553a9a41acee
Simon W <simon@teapotcreative.co.uk>
2020-06-25 16:13:05 +0100

    4.23
    Created a model and the databasce class
    Config file contains all of the database setup

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,9 +1,8 @@
 <?php
 
 class Pages extends Controller {
-    public function __construct()
-    {
-    
+    public function __construct(){
+    $this->postModel = $this->model('Post');
     }
 
     public function index(){
---
+++ app/libraries/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+// PDO Database Class
+// Will connect to DB, create prepared stmts, bind values, return rows and results
+
+class Database{
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    public function __construct(){
+        //set DSN
+        $dsn = 'mysql:host =' . $this->host . ';dbname=' . $this->dbname;
+        $options = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ];
+
+        // Create a PDO instance 
+        try{
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch(PDOException $e){
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+}
