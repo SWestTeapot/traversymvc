7ec189ae54475ee7d11afd1982e44f2036c3f036
Simon W <simon@teapotcreative.co.uk>
2020-06-30 10:08:22 +0100

    4.25 Distro package ready

--- app/config/config.php
+++ app/config/config.php
@@ -3,11 +3,11 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
-define('DB_NAME', 'tmvc');
+define('DB_NAME', '__your__db');
 
 // App root
 define('APPROOT', dirname(__FILE__, 2));
 //URL root
-define('URLROOT', 'http://localhost/traversymvc');
+define('URLROOT', 'http://localhost/__YOUR__ROOT');
 //Site Name
-define ('SITENAME', 'TraversyMVC');
+define ('SITENAME', '__YOUR__SITENAME');
